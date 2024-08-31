831f35aa74cacad7747387c9c08abeac00e1f400
root <root@ip-172-31-34-196.ap-south-1.compute.internal>
2024-08-31 06:51:45 +0000

    first commit

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world\n")
+        self.write("Hello, world...........\n")
         print(self.request)
 
 
